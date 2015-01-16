9e1e473c74c788f3b46785bb0ebd8722c019e531
Wenchao Wang <unlock.liberty@gmail.com>
2015-01-16 15:21:26 -0500

    Let's get the project started!

---
+++ RadioPlayer/FormPlayer.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace RadioPlayer
+{
+    partial class FormPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBoxVolume = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(12, 12);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(371, 20);
+            this.textBoxUrl.TabIndex = 0;
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(12, 38);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(93, 38);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            // 
+            // textBoxVolume
+            // 
+            this.textBoxVolume.Location = new System.Drawing.Point(283, 38);
+            this.textBoxVolume.Name = "textBoxVolume";
+            this.textBoxVolume.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVolume.TabIndex = 3;
+            this.textBoxVolume.Text = "100";
+            this.textBoxVolume.TextChanged += new System.EventHandler(this.textBoxVolume_TextChanged);
+            // 
+            // FormPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 272);
+            this.Controls.Add(this.textBoxVolume);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.textBoxUrl);
+            this.Name = "FormPlayer";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox textBoxVolume;
+    }
+}
+
---
+++ RadioPlayer/FormPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RadioPlayer
+{
+    public partial class FormPlayer : Form
+    {
+        public static WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+
+        public static void PlayMusicFromURL(string url)
+        {
+            player.URL = url;
+
+            player.settings.volume = 100;
+
+            player.controls.play();
+        }
+
+        public static void PlayerStop(string url)
+        {
+            player.controls.stop();
+        }
+
+        public static void SetPlayerVolume(int volume)
+        {
+            player.settings.volume = volume;
+        }
+
+
+        public FormPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            PlayMusicFromURL(textBoxUrl.Text);
+        }
+
+        private void textBoxVolume_TextChanged(object sender, EventArgs e)
+        {
+            SetPlayerVolume(Int32.Parse(textBoxVolume.Text));
+        }
+    }
+}
